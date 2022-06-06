db39b1edf9d0920cc24f04bead185db1742eaab0
benguerard14 <benguerard14@icloud.com>
2022-06-06 16:16:38 -0400

    restart ai

--- Chess/Pieces/pawn.py
+++ Chess/Pieces/pawn.py
@@ -8,15 +8,19 @@ class Pawn(Piece):
         self.piece = "p" if self.color == -1 else "P"
 
     def get_moves(self, board):
-        valid_moves = [] 
-        if board[self.position - 8 * self.color] is None:
-            valid_moves.append(self.position - 8 * self.color)
-            if board[self.position - 16 * self.color] is None and self.position // 8 == (1 if self.color == -1 else 6):
-                valid_moves.append(self.position - 16 * self.color)
-        if board[self.position - 7 * self.color] is not None and board[self.position - 7 * self.color].color != self.color:
-            if self.is_same_diagonal(self.position - 7 * self.color, 1):
-                valid_moves.append(self.position - 7 * self.color)
-        if board[self.position - 9 * self.color] is not None and board[self.position - 9 * self.color].color != self.color:
-            if self.is_same_diagonal(self.position - 9 * self.color, -1):
-                valid_moves.append(self.position - 9 * self.color)
+        valid_moves = []
+        if self.position - 8 >= 0:
+            if board[self.position - 8 * self.color] is None:
+                valid_moves.append(self.position - 8 * self.color)
+                if self.position - 16 >= 0:
+                    if board[self.position - 16 * self.color] is None and self.position // 8 == (1 if self.color == -1 else 6):
+                        valid_moves.append(self.position - 16 * self.color)
+        if self.position - 7 >= 0:
+            if board[self.position - 7 * self.color] is not None and board[self.position - 7 * self.color].color != self.color:
+                if self.is_same_diagonal(self.position - 7 * self.color, 1):
+                    valid_moves.append(self.position - 7 * self.color)
+        if self.position - 9 >= 0:
+            if board[self.position - 9 * self.color] is not None and board[self.position - 9 * self.color].color != self.color:
+                if self.is_same_diagonal(self.position - 9 * self.color, -1):
+                    valid_moves.append(self.position - 9 * self.color)
         return valid_moves
--- main.py
+++ main.py
@@ -2,47 +2,20 @@ import Chess as cs
 import random
 from copy import deepcopy
 
-def minimax(board, is_maximizing, depth):
+def minimax(board, is_maximizing, depth, alpha, beta):
     if cs.game_state(board) != 0 or depth == 0:
         values = cs.return_values(board)
         return values[0] - values[1]
-    pieces = cs.get_pieces(board, not is_maximizing)
-    all_moves = [[piece.position, piece.get_moves(board)] for piece in pieces] 
-    best_value = -100 if is_maximizing else 100
-    best_move = []
-    for piece_move in all_moves:
-        for move in piece_move[1]:
-            new_board = deepcopy(board)
-            new_board[piece_move[0]].move_piece(new_board, move)
-            value = minimax_layers(new_board, not(is_maximizing), depth - 1)
-            if value == best_value:
-                best_move.append([cs.num_to_pos(piece_move[0]), cs.num_to_pos(move)])
-            elif (is_maximizing and value > best_value) or (not is_maximizing and value < best_value):
-                best_value = value
-                best_move.clear()
-                best_move.append([cs.num_to_pos(piece_move[0]), cs.num_to_pos(move)])
-    return random.choice(best_move)
+    pieces = cs.get_pieces(board, is_maximizing)
+    move_values = []
+    for piece in pieces:
+        for move in piece.get_moves(board):
+            new_board = deepcopy(board) 
+            new_board[piece.position].move_piece(new_board, move)
+            move_value = minimax(new_board, not is_maximizing, depth - 1, alpha, beta)
+            move_values.append(move_value)
 
-
-def minimax_layers(board, is_maximizing, depth):
-    if cs.game_state(board) != 0 or depth == 0:
-        values = cs.return_values(board)
-        return values[0] - values[1]
-    pieces = cs.get_pieces(board, not is_maximizing)
-    all_moves = [[piece.position, piece.get_moves(board)] for piece in pieces] 
-    values = []
-    for piece_move in all_moves:
-        for move in piece_move[1]:
-            new_board = deepcopy(board)
-            new_board[piece_move[0]].move_piece(new_board, move)
-            value = minimax_layers(new_board, not(is_maximizing), depth - 1)
-            values.append(value)
-    if is_maximizing:
-        return max(values)
-    else:
-        return min(values)
-            
-        
+         
 def main():
     turn = 0
     turns = [-1, 1]
@@ -50,24 +23,23 @@ def main():
     print("Welcome to Scuffed Chess!")
     while True:
         cs.print_board(board)
-        piece = input("What is the piece you want to move? ")
-        piece = cs.pos_to_num(piece)
-        if board[piece] == None or board[piece].color == turns[turn % 2]:
-            print("Invalid piece")
-            continue
-        move = input("What is the move you want to make? ")
-        move = cs.pos_to_num(move)
-        board[piece].move_piece(board, move)
-        turn += 1
-        print(minimax(board, turn % 2, 3))
-        cs.print_board(board)
-        piece = input("What is the piece you want to move? ")
-        piece = cs.pos_to_num(piece)
-        if board[piece] == None or board[piece].color == turns[turn % 2]:
-            print("Invalid piece")
-            continue
-        move = input("What is the move you want to make? ")
-        move = cs.pos_to_num(move)
+        ai = input("AI? y/n: ")
+        if ai == "y":
+            moves = minimax(board, not (turn % 2), 4, float("inf"), -float("inf"))
+            print(moves)
+            piece = cs.pos_to_num(moves[0])
+            if board[piece] == None or board[piece].color == turns[turn % 2]:
+                print("Invalid piece")
+                continue
+            move = cs.pos_to_num(moves[1])
+        else:
+            piece = input("What is the piece you want to move? ")
+            piece = cs.pos_to_num(piece)
+            if board[piece] == None or board[piece].color == turns[turn % 2]:
+                print("Invalid piece")
+                continue
+            move = input("What is the move you want to make? ")
+            move = cs.pos_to_num(move)
         board[piece].move_piece(board, move)
         turn += 1
 
